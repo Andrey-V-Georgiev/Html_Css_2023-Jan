452deec5d5c58554c0476c118ea1034f92a2c345
Andrey Georgiev <andrey.v.georgiev@gmail.com>
2023-01-21 11:21:42 +0200

    05-CSS-Typography initial

--- utils/fileNamesUtil.js
+++ utils/fileNamesUtil.js
@@ -3,10 +3,19 @@ import { readFile } from 'fs/promises'
 async function example() {
   try {
     const data = await readFile('utils\\exerciseTitles.txt', { encoding: 'utf8' })
-    const dataReplaced = data.replace(/[ &]/g, '-').replace(/[.]/g, '')
-    const splitedData = dataReplaced.split(/\r?\n/)
+    
+    const replace1 = data.replace(/[ &]/g, '-') 
+
+    const replace2 = replace1.replace(/(?<=[0-9])\.(?=[-])/g, '')
+   
+    const replace3 = replace2.replace(/(?<=[0-9])-/g, '.')
+
+    const splitedData = replace3.split(/\r?\n/)
+    
     const joinedData = splitedData.join(' ')
+    
     const mkdirCommand = `mkdir ${joinedData}`
+    
     console.log(mkdirCommand)
   } catch (err) {
     console.log(err)
